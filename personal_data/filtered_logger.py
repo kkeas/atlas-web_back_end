a3148ebd8139e98fb2a6fe3fae68eeb5340684f4
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-18 09:55:03 -0600

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -20,3 +21,20 @@ def filter_datum(fields: List[str],
         )
 
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        OG_message = filter_datum(self.fields, self.REDACTION,
+                                    super().format(record), self.SEPARATOR)
+        return OG_message
