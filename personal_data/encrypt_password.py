b5ba75da122d425780bfa300e474df8203d67669
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-23 22:15:23 -0600

    typo

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env Python3
 """encrypt password"""
 
-import bycrypt
+import bcrypt
 
 def hash_password(password: str) -> bytes:
     """hash a password"""
 
-    hashed = bycrypt.hashpw(password.encode('utf-8'), bycrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bycrypt.gensalt())
     return hashed
