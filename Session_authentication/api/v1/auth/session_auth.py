f3307d3f9effb7dafaa7e44dcd50632478371dff
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-30 17:49:09 -0600

    task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,26 @@
 """session authorization class"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """validate if everything inherits correctly without any overloading
         validate the “switch” by using environment variables"""
-    pass
+
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id=None):
+        """create a session"""
+
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        SessionAuth.user_id_by_session_id[user_id] = session_id
+
+        return session_id
