b564f61e60c45e514965a5c2bed3bca7cea72148
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-29 20:15:26 -0600

    documentation

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,7 @@ from typing import List, TypeVar
 class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method to require authorization"""
         return False
 
     def authorization_header(self, request=None) -> str:
