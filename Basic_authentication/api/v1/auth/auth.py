7389141daebf2feedf443954bd9ee6e5b554ec66
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-29 20:14:02 -0600

    task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env Python3
+
+""" this is a comment """
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """returns auth header"""
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns current user"""
+        return None
