0e6b9360456425381be7b405525a6c46b08eb97d
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-29 22:48:46 -0600

    task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """Basic Auth class file"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,22 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+          self, base64_authorization_header: str
+          ) -> str:
+        """decode a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            decoded_bytes = base64.b64decode(base64_bytes)
+            decoded_utf8 = decoded_bytes.decode('utf-8')
+            return decoded_utf8
+
+        except (ValueError, TypeError, base64.binascii.Error):
+            return None
