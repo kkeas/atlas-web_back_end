daf0a403500d064dcaa21c21a1c36d581baf708b
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-29 20:59:48 -0600

    space typo

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -17,7 +17,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 
-if os.environ.get('AUTH_TYPE') == 'basic_ auth':
+if os.environ.get('AUTH_TYPE') == 'basic_auth':
     auth = BasicAuth()
 else:
     auth = Auth()
