92fbff717a465ab076ff2ab30589db6443c73dc3
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-29 19:17:02 -0600

    pycodestyle app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -20,6 +20,7 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """user is authenticated but not allowed access"""
