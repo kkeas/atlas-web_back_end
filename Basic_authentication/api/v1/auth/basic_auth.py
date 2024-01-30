47626b5714256220b558fda6aefb4d6b8202fa4a
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-29 22:56:26 -0600

    task 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -38,3 +38,20 @@ class BasicAuth(Auth):
 
         except (ValueError, TypeError, base64.binascii.Error):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """returns the user email and password from the Base64 decoded value"""
+
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        credentials = decoded_base64_authorization_header.split(":")
+        return credentials[0], credentials[1]
