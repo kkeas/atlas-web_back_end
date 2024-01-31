9846b1c459eba278784de0a73802ac5420c903cc
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-30 18:39:47 -0600

    task 5

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -35,15 +35,20 @@ def before_request():
     excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
     ]
 
-    if auth.require_auth(request.path, excluded_paths):
-        request.current_user = auth.current_user(request)
-        if auth.authorization_header(request) is None:
-            return abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) and \
+            auth.session_cookie(request) is None:
+        return abort(401)
+    if auth.current_user(request) is None:
+        return abort(403)
+
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
