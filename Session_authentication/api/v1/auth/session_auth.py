9a22581dbb05dd3001df2bbf200f607521c6456e
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-01-30 18:24:38 -0600

    task 3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
 
     user_id_by_session_id = {}
 
-    def create_session(self, user_id=None):
+    def create_session(self, user_id: str = None) -> str:
         """create a session"""
 
         if user_id is None:
@@ -24,3 +24,10 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Method for retrieving a User ID by session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
