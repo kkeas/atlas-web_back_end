fc806e14a72c187e86fe655432eaf3ef4f4df2f5
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-02-27 17:27:20 -0600

    stubs

---
+++ unittests_in_js/4-payment.js
@@ -0,0 +1,9 @@
+// payment
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const result = 10;
+  console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
