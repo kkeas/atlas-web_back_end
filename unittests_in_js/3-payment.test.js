500a9453770c77a29a8bf76fc2afede108f03b5a
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-02-27 16:23:26 -0600

    forgot to commit a file

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,26 @@
+// test payment.js
+const chai = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+const expect = chai.expect;
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with SUM type and log the result', function () {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    // make sure it was called correctly
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+
+    // make sure return was logged to the console
+    expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
+
+    // Restore to prevent side effects
+    calculateNumberSpy.restore();
+    consoleLogSpy.restore();
+  });
+});
