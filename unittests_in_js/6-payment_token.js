57a28724359a80dc895bd04ad85fd14b56c987c0
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-02-27 19:01:21 -0600

    change

---
+++ unittests_in_js/6-payment_token.js
@@ -0,0 +1,12 @@
+// Async tests with done
+
+
+function getPaymentTokenFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({data: 'Successful response from the API'});
+        }
+    });
+}
+
+module.exports = getPaymentTokenFromAPI;
--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -1,17 +1,15 @@
 // Async tests
 
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe('getPaymentTokenFromAPI', () => {
-  it('should return a resolved promise with the correct value when success is true', (done) => {
-    getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).to.deep.equal({ data: 'Successful response from the API' });
-        done();
-      })
-      .catch(error => {
-        done(error);
-      })
-  })
-})
+describe('getPaymentTokenFromAPI', function () {
+    it('should return a resolved promise', function (done) {
+        getPaymentTokenFromAPI(true)
+            .then((data) => {
+                expect(data).to.deep.equal({ data: 'Successful response from the API' });
+                done();
+            })
+            .catch((error) => done(error));
+    });
+});
