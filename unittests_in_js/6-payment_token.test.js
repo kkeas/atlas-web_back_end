f190ef02d5da91c0e7845c4fd7fa6e6166605c76
katrina keas <katrinakeas@katrinas-MacBook-Air.local>
2024-02-27 18:58:23 -0600

    async tests with done

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,17 @@
+// Async tests
+
+const expect = require('chai').expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with the correct value when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch(error => {
+        done(error);
+      })
+  })
+})
